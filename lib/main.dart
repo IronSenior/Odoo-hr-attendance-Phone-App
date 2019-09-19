84416cf5935f80e9fc0dc838c9e1edabea6e3d0d
Pepe <pepemarquezof@gmail.com>
2019-09-19 19:36:53 +0200

    It works now

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:odoo_api/odoo_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+
+Future main() async {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,11 +33,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _employeeIn = false; 
+  bool _employeeIn = false;
+  var _client;
+  var _user;
 
-  void _incrementCounter() {
-    setState(() {
-      _employeeIn = ! _employeeIn;
+  void _sign() async {
+    final prefs = await SharedPreferences.getInstance();
+    _client = new OdooClient((prefs.getString("Odoo_URL") ?? ""));
+
+    _client.authenticate(prefs.getString("User_Name"), prefs.getString("Password"), prefs.getString("Odoo_Database")).then((auth) {
+        if(auth.isSuccess) {
+          _user = auth.getUser();
+          _client.searchRead("hr.employee", [["user_id", "=", auth.getUser().uid]], ["id"]).then((employeeResult) {
+            if(! employeeResult.hasError()){
+              var _employee = employeeResult.getResult()["records"][0]["id"];
+              _client.callKW("hr.employee", "attendance_manual", [_employee, "hr_attendance.hr_attendance_action_my_attendances"]).then((kwResult) {
+                if(! kwResult.hasError()){
+                  setState(() {
+                    _employeeIn = ! _employeeIn;
+                  });
+                }
+              });
+            }
+          });
+        }
     });
   }
 
@@ -55,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: IconButton(
                             icon: (_employeeIn) ? Icon( FontAwesomeIcons.signOutAlt, color: Colors.white, size: 100)
                                                     : Icon(FontAwesomeIcons.signInAlt, color: Colors.white, size: 100),
-                            onPressed: _incrementCounter
+                            onPressed: _sign
                           ),
                 width: 200,
                 height: 200,
@@ -89,12 +114,18 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsState extends State<SettingsPage> {
 
-  String _odooUrl = "";
+  
   bool _connected = false;
   var _client;
+  final _formKey = GlobalKey<FormState>();
+  final _urlController = TextEditingController();
+  final _userNameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _databaseController = TextEditingController();
   
-  void _checkConnectionState() {
-    _client = new OdooClient(_odooUrl);
+  void _checkConnectionState() async {
+    final prefs = await SharedPreferences.getInstance();
+    _client = new OdooClient((prefs.getString("Odoo_URL") ?? "http://82.223.32.157:8071"));
     _client.connect().then((version) {
       setState(() {
         _connected = true;
@@ -114,22 +145,93 @@ class _SettingsState extends State<SettingsPage> {
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title)
+        title: Text(widget.title),
       ),
-      body: Center(
+      body: Form(
+        key: _formKey,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Container(
-              color: (_connected) ? Colors.green
-                                    :Colors.red,
-              child: Icon(FontAwesomeIcons.plug),
-              width: MediaQuery.of(context).size.width,
-              height: 30,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Odoo URL'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _urlController,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Database'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _databaseController,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Login'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _userNameController,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _passwordController,
+                ),
+              ],
             ),
-          ],
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () async {
+                    final prefs = await SharedPreferences.getInstance();
+                    if (_formKey.currentState.validate()) {
+                      prefs.setString("Odoo_URL", _urlController.text);
+                      prefs.setString("Odoo_Database", _databaseController.text);
+                      prefs.setString("User_Name", _userNameController.text);
+                      prefs.setString("Password", _passwordController.text);
+                    }
+                  },
+                  child: Text('Save'),
+                ),
+                Container(
+                  color: (_connected) ? Colors.green
+                                        :Colors.red,
+                  child: Icon(FontAwesomeIcons.plug),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                ),
+              ],
+            ),
+          ]
         ),
-      ),
+      )
     );
   }
   
